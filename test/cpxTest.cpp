370048bea065ea2de4453e676eeac80535537d9f
Phil Ahrenkiel <61021639+spahrenk@users.noreply.github.com>
2026-01-12 17:15:37 -0700

    Add tests.

---
+++ test/cpxTest.cpp
@@ -0,0 +1,22 @@
+/* Copyright (c) 2024 Big Ladder Software LLC. All rights reserved.
+ * See the LICENSE file for additional terms and conditions. */
+
+// unit test
+#include "unit-test.hpp"
+
+// tlbx
+#include "cpx.hpp"
+
+/*
+ * cpx tests
+ */
+TEST(cpxTest, general)
+{
+    {
+        cpx z(1., 2.);
+        EXPECT_EQ(z.Re(), 1.);
+        EXPECT_EQ(z.Im(), 2.);
+    }
+
+  
+}
